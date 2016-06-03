71451b866d7898514d742554297188ec8df47792
M7mmdgMeR7 <Mohalaamer@icloud.com>
2016-06-03 12:08:29 +0300

    Create Main.php

---
+++ Src/Heal-plugin/Main.php
@@ -0,0 +1,13 @@
+<?php
+
+Namespace; Heal-plugin
+
+use Pocketmine\Plugin\PluginBase
+
+use Pocketmine\command\Command
+
+use Pocketmine\command\CommandSender
+
+class main extends PluginBase{
+
+Public functions 
