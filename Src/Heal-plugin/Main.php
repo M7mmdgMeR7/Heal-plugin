711c397365ffab8e058c0a717ad2d0b9cf568a18
M7mmdgMeR7 <Mohalaamer@icloud.com>
2016-06-03 13:53:11 +0300

    Update Main.php

--- Src/Heal-plugin/Main.php
+++ Src/Heal-plugin/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-Namespace; Heal-plugin
+Namespace; Heal_plugin
 
 use Pocketmine\Plugin\PluginBase
 
@@ -10,4 +10,31 @@ use Pocketmine\command\CommandSender
 
 class main extends PluginBase{
 
-Public functions 
+Public function onEnable(){
+
+  $this->getserver()->getLogger()->info("Heal plugin is enable");
+   }
+
+         public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+
+ case 'heal':
+                      $sender->setHealth(20);
+ break;
+           case 'message':
+ $this->getServer()->broadcast($args[you are healing])
+     break;
+            case 'cre1':
+     $player = $sender->getPlayer();
+                                        $player->setGamemode(1);
+              break; 
+            case 'sur0':
+    $player = $sender->getPlayer();
+               $player->setGamemode(0);
+   break;
+            case 'adv3':
+     $player = $sender->getPlayer();
+$player->setGamemode(3);
+      
+
+
